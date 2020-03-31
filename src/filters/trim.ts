0be9afbc01f4d4356b3f09865cc0c499d9a2e45b
Tuan Nguyen <tuan.nguyen@nodeplusplus.com>
2020-03-31 14:19:13 +0700

    clean up

--- src/filters/trim.ts
+++ src/filters/trim.ts
@@ -1,16 +1,11 @@
 import { IXFilterFunction } from "../types/XFilter";
 
-const trim: IXFilterFunction<
-  string,
-  any,
-  IXFilterFunctionTrimOpts
-> = function trim(payload: any, opts?: IXFilterFunctionTrimOpts): string {
+const trim: IXFilterFunction<string, any> = function trim(
+  payload: any
+): string {
   if (!payload || typeof payload !== "string") return payload;
 
   return payload.trim();
 };
 
 export default trim;
-export interface IXFilterFunctionTrimOpts {
-  force: boolean;
-}
